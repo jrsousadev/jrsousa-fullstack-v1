9e243029951463ffc47bee3cc015527366e70398
Junior Sousa <jrsousadev@gmail.com>
2022-08-23 01:53:37 -0300

    (main): added theme provider in myapp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeContextProvider } from "../contexts/ThemeContext";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
