113c915a2c0284b8013d61636eba1941acd5621a
Junior Sousa <jrsousadev@gmail.com>
2022-08-23 14:35:14 -0300

    (main): create cardstack and css

---
+++ src/components/CardStack/index.tsx
@@ -0,0 +1,66 @@
+import { Box, chakra, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { BiStar } from "react-icons/bi";
+import { GrMysql, GrReactjs } from "react-icons/gr";
+import {
+  SiJavascript,
+  SiMongodb,
+  SiNextdotjs,
+  SiPostgresql,
+  SiTypescript,
+} from "react-icons/si";
+import { IoLogoNodejs } from "react-icons/io";
+import { isValidMotionProp, motion } from "framer-motion";
+
+import styles from "./CardStack.module.scss";
+
+type CardStackProps = {
+  stack: string;
+  isMainStack: boolean;
+};
+
+const ChakraDiv = chakra(motion.div, {
+  shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === "children",
+});
+
+export const CardStack = ({ isMainStack, stack }: CardStackProps) => {
+  useEffect(() => {}, []);
+
+  const returnStack = () => {
+    switch (stack) {
+      case "react":
+        return <GrReactjs className={styles.iconStack} />;
+      case "typescript":
+        return <SiTypescript className={styles.iconStack} />;
+      case "next":
+        return <SiNextdotjs className={styles.iconStack} />;
+      case "node":
+        return <IoLogoNodejs className={styles.iconStack} />;
+      case "mongo":
+        return <SiMongodb className={styles.iconStack} />;
+      case "postgress":
+        return <SiPostgresql className={styles.iconStack} />;
+      case "mysql":
+        return <GrMysql className={styles.iconStack} />;
+      case "javascript":
+        return <SiJavascript className={styles.iconStack} />;
+    }
+  };
+
+  return (
+    <ChakraDiv
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={"all 0.5s"}
+    >
+      <Box className={styles.BoxContainer}>
+        {isMainStack && (
+          <Text className={styles.MainStack}>
+            <BiStar size={17} /> Main Stack
+          </Text>
+        )}
+        <Box className={styles.BoxContent}>{returnStack()}</Box>
+      </Box>
+    </ChakraDiv>
+  );
+};
