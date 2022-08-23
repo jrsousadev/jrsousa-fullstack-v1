fb299f4d1c9908eccb9f5a0b1a8160afa70bb159
Junior Sousa <jrsousadev@gmail.com>
2022-08-23 11:31:03 -0300

    (main): create component particles

---
+++ src/components/Particle.tsx
@@ -0,0 +1,63 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import styles from "./Particle.module.scss";
+
+export const Particle = () => {
+  const particlesInit = useCallback(async (engine: any) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: any) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      id={styles.tsparticles}
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fpsLimit: 60,
+        particles: {
+          color: {
+            value: "#c3d8dc",
+          },
+          links: {
+            color: "#c3d8dc",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
