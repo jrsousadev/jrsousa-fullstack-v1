df490018c1a94ccfbd0d86683d115d21de0640ec
Junior Sousa <jrsousadev@gmail.com>
2022-08-23 14:35:45 -0300

    (main): fix css globals and configurate page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,159 @@
 import type { NextPage } from "next";
-import { chakra, Text } from "@chakra-ui/react";
+import { Button, chakra, Text, useColorMode } from "@chakra-ui/react";
 import { isValidMotionProp, motion } from "framer-motion";
+import { Particle } from "../components/Particle";
+import { useEffect, useState } from "react";
 
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
-import { Particle } from "../components/Particle";
+import { CardStack } from "../components/CardStack";
+import { myStacksBack, myStacksFront } from "../constants/ListMyStacks";
 
-const ChakraText = chakra(motion.div, {
+const ChakraDiv = chakra(motion.div, {
   shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === "children",
 });
 
 const Home: NextPage = () => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [cursorVariant, setCursorVariant] = useState("default");
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    const mouseMove = (e: any) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+    options: {
+      opacity: 0.5,
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+  };
+
+  const enterMouseOptions = () => {
+    setCursorVariant("options");
+  };
+
+  const leaveMouseOptions = () => {
+    setCursorVariant("default");
+  };
+
   return (
     <>
       <Head>
         <title>Jr. Sousa | FULL-STACK</title>
       </Head>
 
+      <motion.div
+        className={styles.cursor}
+        variants={variants}
+        animate={cursorVariant}
+      />
+
       <header className={styles.containerHeader}>
+        <Button onClick={toggleColorMode}>
+          Toggle {colorMode === "light" ? "Dark" : "Light"}
+        </Button>
         <div className={styles.optionsMenuContainer}>
-          <ChakraText
-            className={styles.textMenu}
+          <ChakraDiv
             animate={{
               scale: [2, 1],
             }}
           >
-            Stacks
-          </ChakraText>
-          <ChakraText
-            className={styles.textMenu}
+            <i
+              className={styles.textMenu}
+              onMouseEnter={enterMouseOptions}
+              onMouseLeave={leaveMouseOptions}
+            >
+              Stacks
+            </i>
+          </ChakraDiv>
+          <ChakraDiv
             animate={{
               scale: [2, 1],
             }}
           >
-            Portfólio
-          </ChakraText>
-          <ChakraText
-            className={styles.textMenu}
+            <i
+              className={styles.textMenu}
+              onMouseEnter={enterMouseOptions}
+              onMouseLeave={leaveMouseOptions}
+            >
+              Porfólio
+            </i>
+          </ChakraDiv>
+          <ChakraDiv
             animate={{
               scale: [2, 1],
             }}
           >
-            Contato
-          </ChakraText>
+            <i
+              className={styles.textMenu}
+              onMouseEnter={enterMouseOptions}
+              onMouseLeave={leaveMouseOptions}
+            >
+              Contato
+            </i>
+          </ChakraDiv>
         </div>
       </header>
 
       <main>
-        <section className={styles.containerSectionOne}>
+        <section className={styles.containerSectionTitles}>
           <Particle />
           <div className={styles.containerTitle}>
-            <ChakraText className={styles.title} animate={{}}>
+            <ChakraDiv className={styles.title} animate={{}}>
               Junior Sousa
-            </ChakraText>
+            </ChakraDiv>
             <Text className={styles.subtitle}>Desenvolvedor</Text>
             <Text className={styles.subtitle}>Full-stack</Text>
             <Text className={styles.paragraph}>
               Profissional especialista em desenvolvimento web, focado nas
               stacks React e NodeJS, autêntico e curioso.
             </Text>
+          </div>
+        </section>
+
+        <section className={styles.containerSectionStacks}>
+          <Text className={styles.title}>Front</Text>
+
+          <div className={styles.gridStacks}>
+            {myStacksFront.map((stack, index) => (
+              <CardStack
+                isMainStack={stack.isMainStack}
+                stack={stack.name}
+                key={index}
+              />
+            ))}
+          </div>
+
+          <Text className={styles.title}>Back</Text>
+
+          <div className={styles.gridStacks}>
+            {myStacksBack.map((stack, index) => (
+              <CardStack
+                isMainStack={stack.isMainStack}
+                stack={stack.name}
+                key={index}
+              />
+            ))}
           </div>
         </section>
       </main>
