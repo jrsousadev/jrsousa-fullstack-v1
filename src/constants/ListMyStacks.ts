e0e14e1039712f9bac0049cd073d268e85367220
Junior Sousa <jrsousadev@gmail.com>
2022-08-23 14:35:24 -0300

    (main): create my list stacks

---
+++ src/constants/ListMyStacks.ts
@@ -0,0 +1,12 @@
+export const myStacksFront = [
+  { name: "react", isMainStack: true },
+  { name: "next", isMainStack: true },
+  { name: "typescript", isMainStack: true },
+  { name: "javascript", isMainStack: false },
+];
+
+export const myStacksBack = [
+  { name: "node", isMainStack: true },
+  { name: "mongo", isMainStack: true },
+  { name: "typescript", isMainStack: true },
+];
