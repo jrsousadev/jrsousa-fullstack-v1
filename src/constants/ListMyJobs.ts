9ce0be7659bc19400e81316995698c609b5f61e5
Junior Sousa <jrsousadev@gmail.com>
2022-08-24 01:24:11 -0300

    (main): create list my jobs

---
+++ src/constants/ListMyJobs.ts
@@ -0,0 +1,62 @@
+export const myJobs = [
+  {
+    name: "Unitok",
+    stacks: ["Next", "Node", "Typescript", "Aws", "Css-In-Js", "MongoDB"],
+    link: "https://www.unitok.com/",
+    github: "",
+    image: "https://i.imgur.com/cT57xDu.png",
+    description: ` Responsável pela plataforma de produtos digitais Unitok. Minhas
+    principais funções: apresentar e desenvolver soluções para novas
+    funcionalidades, manter um padrão de desenvolvimento escalável,
+    discutir sobre a experiência do usuário junto ao time de design,
+    conduzir e auxiliar a atuação de novos desenvolvedores na equipe.
+    Atuando proativamente dando novas ideias de melhorias para o P.O. e
+    fazendo auto-gerenciamento de tempo.`,
+  },
+  {
+    name: "Conarh + Unitok",
+    stacks: ["Next", "Node", "Typescript", "Aws", "Css-In-Js", "MongoDB"],
+    link: "https://www.unitok.com/conarh2022",
+    github: "",
+    image: "https://i.imgur.com/y2B9dD7.png",
+    description: ` Responsável pela plataforma de controle de mailing dos expositores
+     do evento CONARH2022 organizado pela ABRH BRASIL. Minhas principais 
+     funções: participar da concepção da solução, discutindo fluxos e
+      garantindo uma boa experiência do usuário junto ao time de design, 
+      apresentar e desenvolver as funcionalidades da plataforma, dar 
+      assistência e correções ágeis em tempo real, conduzir e organizar a 
+      equipe de desenvolvimento.`,
+  },
+  {
+    name: "Customer Control",
+    stacks: ["Next", "Node", "Typescript", "Css-In-Js", "MongoDB"],
+    link: "https://www.unitok.com/conarh2022",
+    github: "https://github.com/jrsousadev/Customer_Control_web",
+    image: "https://i.imgur.com/STLes2R.png",
+    description: `Projeto desenvolvido por mim, criado com o intuito de 
+    resolver problemas/dificuldades de amigos de trabalho (e até minha mesmo) 
+    com controle de seus clientes e datas de pagamentos.`,
+  },
+  {
+    name: "Clinica Apit Fhisio",
+    stacks: ["Next", "Node", "Typescript", "Css-In-Js", "MongoDB"],
+    link: "http://clinicaapitfhisio.com.br/professor/login",
+    github: "https://github.com/jrsousadev/Apit_Fhisio_web",
+    image: "https://i.imgur.com/8NrdBhc.png",
+    description: `Sistema de clínica (REAL), desenvolvido com o objetivo de cadastrar
+    pacientes, criar matrículas e ter o controle dos vencimentos de pagamento, além de
+    ter os dados dos pacientes e quais são suas matrículas, sistema com autênticação e
+    dados relacional NoSQL (MongoDB)`,
+  },
+  {
+    name: "Challenge Suflex Full-stack",
+    stacks: ["Next", "Node", "Typescript", "Css-In-Js", "Postgress"],
+    link: "https://www.unitok.com/conarh2022",
+    github: "https://github.com/jrsousadev/challenge-surflex-front",
+    image: "https://i.imgur.com/ZbERIMZ.png",
+    description: `Challenge Full-stack nível pleno, neste projeto é possível
+    realizar um registro/autênticação de usuário, filtrar personagens, e criar 
+    própria lista de personagens favoritos (salvando em banco de dados) , 
+    consumindo API visando a performance do projeto e a experiência do usuário.`,
+  },
+];
