c6ca470826708a92e7c58474534b86847f71a508
Junior Sousa <jrsousadev@gmail.com>
2022-08-24 14:27:31 -0300

    (main): added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 import { theme } from "../contexts/ThemeContext";
+import favicon from "../public/favicon.ico";
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-br">
         <Head>
           <meta charSet="utf-8" />
-
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
