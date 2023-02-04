6edc20b5fabdf826285aa0ee4254896bdb5d799e
Junior Sousa <jrsousadev@gmail.com>
2023-02-04 19:48:50 -0300

    refactoring portfólio

--- src/constants/ListMyJobs.ts
+++ src/constants/ListMyJobs.ts
@@ -1,131 +1,75 @@
-export const myJobs = [
+import LogoBeWell from "../../public/assets/logo-bewell.png";
+import LogoBotCheckin from "../../public/assets/logo-botcheckin.png";
+import LogoUnitok from "../../public/assets/logo-unitok.png";
+import LogoUnitokConarh from "../../public/assets/logo-unitok-conarh.png";
+
+import { StaticImageData } from "next/image";
+
+export type JobProps = {
+  name: string;
+  slogan: string;
+  link: string;
+  image: StaticImageData;
+  imageWidth: number;
+  imageHeight: number;
+  description: string;
+  period?: string;
+};
+
+export const myJobs: JobProps[] = [
   {
-    name: "Be Well (atual)",
-    stacks: ["React", "Node", "Javascript", "AWS", "Css-In-Js", "MysQL"],
+    name: "Be Well",
+    slogan: "Soluções tecnológicas",
     link: "http://usobewell.com/",
-    github: "",
-    image: "https://i.imgur.com/wSTx1s2.png",
+    image: LogoBeWell,
+    imageWidth: 110,
+    imageHeight: 140,
+    period: "Oct 2022 - Present",
     description: `
-      Responsável pelo Sistema "Processo Hospitalar" Be Well. Minhas principais funções:
-      Desenvolvimento de novas features, correção de bugs e erros de lógica, construir arquitetura
-      de Front-end/Back-end de acordo com novas funcionalidades e necessidades, manter um padrão
-      de desenvolvimento escalável e auxiliar a atuação de novos desenvolvedores na equipe.
+      Responsável pelo desenvolvimento do sistema cujo objetivo é desenvolver nossas features de acordo
+      com a necessidade do cliente, correção de bugs e automação de processos hospitalares e também gerenciamento
+      e monitoramento de novos desenvolvedores.
     `,
   },
   {
-    name: "BOT Checkin (Founder)",
-    stacks: ["Next", "Node", "MongoDB", "Css-In-Js", "Prisma"],
+    name: "BotCheckin",
+    slogan: "Chatbot",
     link: "https://conheca-bot-checkin.vercel.app/",
-    github: "",
-    image: "https://i.imgur.com/fOQJE0f.png",
-    description: `
-      O BOT CHECK-IN surgiu para revolucionar o mercado de automações de 
-      CHATBOT para WhatsApp, e finalmente, viemos para oportunizar que os 
-      profissionais que trabalham coletando check-ins/relatórios de TODOS OS SEUS 
-      CLIENTES via WhatsApp tivessem um assistente que o ajuda-se nessa rotina fazendo 
-      com que o mesmo ganha-se mais tempo e qualidade de vida.
+    period: "Dec 2022 - Present",
+    image: LogoBotCheckin,
+    imageWidth: 150,
+    imageHeight: 150,
+    description: ` 
+      CTO and Founder, sistema desenvolvido com o intuito inicial de resolver um problema de um amigo próximo,
+      que após suprir suas necessidades decidimos lançar como um produto Saas, sistema integrado com WhatsApp API, pegando dados recebidos no WhatsApp e exibindo na plataforma web.
     `,
   },
   {
     name: "Unitok",
-    stacks: ["Next", "Node", "Typescript", "Aws", "Css-In-Js", "MongoDB"],
+    slogan: "Cartões digital",
     link: "https://www.unitok.com/",
-    github: "",
-    image: "https://i.imgur.com/ta6tDNH.png",
-    description: ` Responsável pela plataforma de produtos digitais Unitok. Minhas
-    principais funções: apresentar e desenvolver soluções para novas
-    funcionalidades, manter um padrão de desenvolvimento escalável,
-    discutir sobre a experiência do usuário junto ao time de design,
-    conduzir e auxiliar a atuação de novos desenvolvedores na equipe.
-    Atuando proativamente dando novas ideias de melhorias para o P.O. e
-    fazendo auto-gerenciamento de tempo.`,
+    image: LogoUnitok,
+    imageWidth: 350,
+    imageHeight: 150,
+    period: "Jan 2022 - Sep 2022",
+    description: `
+      Responsável pelo desenvolvimento de novos produtos, discutir sobre experiência do usuário junto ao 
+      time de design, melhorar código aplicando conceitos de DDD, CLEAN CODE e SOLID, conduzir e auxiliar a atuação de novos desenvolvedores.
+    `,
   },
   {
-    name: "Conarh + Unitok",
-    stacks: ["Next", "Node", "Typescript", "Aws", "Css-In-Js", "MongoDB"],
+    name: "Unitok",
+    slogan: "Conarh",
     link: "https://www.unitok.com/conarh2022",
-    github: "",
-    image: "https://i.imgur.com/hBM6uOu.png",
-    description: ` Responsável pela plataforma de controle de mailing dos expositores
-     do evento CONARH2022 organizado pela ABRH BRASIL. Minhas principais 
-     funções: participar da concepção da solução, discutindo fluxos e
-      garantindo uma boa experiência do usuário junto ao time de design, 
-      apresentar e desenvolver as funcionalidades da plataforma, dar 
-      assistência e correções ágeis em tempo real, conduzir e organizar a 
-      equipe de desenvolvimento.`,
-  },
-  {
-    name: "DR. Felipe Farah",
-    stacks: ["React", "Typescript", "Css-In-Js", "Front-end"],
-    link: "https://drfelipefarah.com.br",
-    image: "https://i.imgur.com/rUQylyr.png",
-    description: `(Freelance) Landing Page, na mesma é exibido uma biografia sobre o profissional, quais foram seus feitos, 
-    resultados e também seus cursos ao vivo juntamente com seus alunos.`,
-  },
-  {
-    name: "BOT Checkin (LP)",
-    stacks: ["Next", "Typescript", "Css-In-Js", "Front-end"],
-    link: "https://conheca-bot-checkin.vercel.app",
-    image: "https://i.imgur.com/tk4bZ8K.png",
-    description: `      O BOT CHECK-IN surgiu para revolucionar o mercado de automações de 
-    CHATBOT para WhatsApp, e finalmente, viemos para oportunizar que os 
-    profissionais que trabalham coletando check-ins/relatórios de TODOS OS SEUS 
-    CLIENTES via WhatsApp tivessem um assistente que o ajuda-se nessa rotina fazendo 
-    com que o mesmo ganha-se mais tempo e qualidade de vida.`,
-  },
-  {
-    name: "Live chat",
-    stacks: ["Node", "Typescript", "Supertest", "Jest", "Prisma", "MongoDB"],
-    github: "https://github.com/jrsousadev/api-pagarme-nodejs",
-    link: "https://live-chat-web.vercel.app/637d9d6fcaeea7b8cd7d7fed",
-    image: "https://i.imgur.com/0JJOysy.png",
-    description: `
-      Projeto criado com o intuito de melhorar minhas habilidades, no mesmo foi
-      desenvolvido um pequeno sistema inspirando-se no WhatsApp de forma rasa, desde
-      criação da API e de testes automotizados.
+    image: LogoUnitokConarh,
+    imageWidth: 250,
+    imageHeight: 150,
+    period: "Jan 2022 - Sep 2022",
+    description: ` 
+      Responsável pelo desenvolvimento do total zero da plataforma de controle de mailing dos expositores
+      do evento CONARH 2022, organizado pela ABRH BRASIL cujo meu objetivo era discutir fluxos garantindo
+      uma boa experiência do usuário junto ao time de design, auxiliar desenvolvedores, dar assistência e 
+      correções ágeis em tempo real.
     `,
-  },
-  {
-    name: "API PagarMe NodeJS",
-    stacks: ["Node", "Typescript", "Swagger", "PagarMe", "Solid", "MongoDB"],
-    github: "https://github.com/jrsousadev/api-pagarme-nodejs",
-    image:
-      "https://partnernetwork.vtexassets.com/arquivos/ids/155570/logo_pagarme.png?v=637599700153200000",
-    description: `
-      Projeto desenvolvido com o intuito de ajudar desenvolvedores
-      com a implementação da API do PagarMe (Pagamentos e transações) com Typescript no NodeJS e 
-      utilizando o banco de dados MongoDB. 
-    `,
-  },
-  {
-    name: "Full Calendar Scheduler",
-    stacks: ["Next", "Typescript", "Css-In-Js", "MongoDB"],
-    link: "https://fullcalendar-scheduler-web.vercel.app/",
-    github: "https://github.com/jrsousadev/fullcalendar-scheduler-web",
-    image: "https://i.imgur.com/f2RukgD.png",
-    description: `Componente inspirado no Google Agenda, o mesmo foi criado
-    com o intuito de ajudar pessoas que necessitem das funcionalidades prontas de listar/criar/atualizar/delete
-    eventos tanto no front-end quanto integrado ao back-end. (Open Source)`,
-  },
-  {
-    name: "Customer Control",
-    stacks: ["Next", "Node", "Typescript", "Css-In-Js", "MongoDB"],
-    link: "https://customer-control-web.herokuapp.com/",
-    github: "https://github.com/jrsousadev/Customer_Control_web",
-    image: "https://i.imgur.com/cD8Wj93.png",
-    description: `Projeto desenvolvido por mim, criado com o intuito de 
-    resolver problemas/dificuldades de amigos de trabalho (e até minha mesmo) 
-    com controle de seus clientes e datas de pagamentos.`,
-  },
-  {
-    name: "Clinica Apit Fhisio",
-    stacks: ["Next", "Node", "Typescript", "Css-In-Js", "MongoDB"],
-    link: "http://clinicaapitfhisio.com.br/professor/login",
-    github: "https://github.com/jrsousadev/Apit_Fhisio_web",
-    image: "https://i.imgur.com/Lw7Ko0h.png",
-    description: `Sistema de clínica (REAL), desenvolvido com o objetivo de cadastrar
-    pacientes, criar matrículas e ter o controle dos vencimentos de pagamento, além de
-    ter os dados dos pacientes e quais são suas matrículas, sistema com autênticação e
-    dados (MongoDB)`,
   },
 ];
