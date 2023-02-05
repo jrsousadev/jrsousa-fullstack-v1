aa9a92e09a38a7728a780760c2501a2b56789c34
Junior Sousa <jrsousadev@gmail.com>
2023-02-05 12:49:53 -0300

    finishing portfolio

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 import { theme } from "../contexts/ThemeContext";
-import favicon from "../public/favicon.ico";
 class MyDocument extends Document {
   render() {
     return (
