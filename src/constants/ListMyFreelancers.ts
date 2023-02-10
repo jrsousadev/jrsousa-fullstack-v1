cd46a1457f45b51c9535a9c2a7642628c49f1143
Junior Sousa <jrsousadev@gmail.com>
2023-02-10 18:41:48 -0300

    fix

--- src/constants/ListMyFreelancers.ts
+++ src/constants/ListMyFreelancers.ts
@@ -3,6 +3,7 @@ import LogoDegeo from "../../public/assets/logo-degeo.png";
 import LogoFelipeFarah from "../../public/assets/logo-felipefarah.png";
 import LogoOpiniaoCerta from "../../public/assets/logo-opiniao.png";
 import LogoMines from "../../public/assets/logo-mines.png";
+import LogoOgDevelopment from "../../public/assets/logo-og.png";
 
 import { StaticImageData } from "next/image";
 
@@ -30,15 +31,15 @@ export const myFreelancers: FreelancersProps[] = [
     `,
   },
   {
-    name: "Mines Lucrativo",
-    slogan: "Robô de apostas",
+    name: "OG Development",
+    slogan: "Landing Page",
     /* link: "", */
-    image: LogoMines,
+    image: LogoOgDevelopment,
     imageWidth: 200,
-    imageHeight: 110,
+    imageHeight: 200,
     description: `
-      Responsável pelo desenvolvimento do robô de apostas Mines Lucrativo, desenvolvido para usuários realizarem
-      suas apostas na F12 Bet.
+      Criação da Landing Page da OG Development, a mesma foi criada com o intuito de captação de novos clientes e apresentação breve de
+      serviços realizado pela mesma.
     `,
   },
   {
@@ -53,7 +54,18 @@ export const myFreelancers: FreelancersProps[] = [
       e ganhar saldo para resgatar prêmios.
     `,
   },
-
+  {
+    name: "Mines Lucrativo",
+    slogan: "Robô de apostas",
+    /* link: "", */
+    image: LogoMines,
+    imageWidth: 200,
+    imageHeight: 110,
+    description: `
+      Responsável pelo desenvolvimento do robô de apostas Mines Lucrativo, desenvolvido para usuários realizarem
+      suas apostas na F12 Bet.
+    `,
+  },
   {
     name: "Degeocollege",
     slogan: "Landing Page",
@@ -62,8 +74,7 @@ export const myFreelancers: FreelancersProps[] = [
     imageWidth: 150,
     imageHeight: 110,
     description: `
-      Criação da Landing Page do Dr. Felipe Farah, aplicando SEO e aplicação de estratégias de marketing
-      para reentação de novos possíveis clientes.
+      Criação da Landing Page da Degeocollege para captação de clientes em lançamento perpétuo.
     `,
   },
   {
@@ -72,7 +83,7 @@ export const myFreelancers: FreelancersProps[] = [
     link: "https://drfelipefarah.com.br/",
     image: LogoFelipeFarah,
     imageWidth: 300,
-    imageHeight: 110,
+    imageHeight: 100,
     description: `
       Criação da Landing Page do Dr. Felipe Farah, aplicando SEO e automação de imagens.
     `,
@@ -88,60 +99,4 @@ export const myFreelancers: FreelancersProps[] = [
       Criação da Landing Page do BotCheckin, aplicando SEO e automação.
     `,
   },
-  /*  
-  {
-    name: "Live chat",
-    stacks: ["Node", "Typescript", "Supertest", "Jest", "Prisma", "MongoDB"],
-    github: "https://github.com/jrsousadev/api-pagarme-nodejs",
-    link: "https://live-chat-web.vercel.app/637d9d6fcaeea7b8cd7d7fed",
-    image: "https://i.imgur.com/0JJOysy.png",
-    description: `
-      Projeto criado com o intuito de melhorar minhas habilidades, no mesmo foi
-      desenvolvido um pequeno sistema inspirando-se no WhatsApp de forma rasa, desde
-      criação da API e de testes automotizados.
-    `,
-  },
-  {
-    name: "API PagarMe NodeJS",
-    stacks: ["Node", "Typescript", "Swagger", "PagarMe", "Solid", "MongoDB"],
-    github: "https://github.com/jrsousadev/api-pagarme-nodejs",
-    image:
-      "https://partnernetwork.vtexassets.com/arquivos/ids/155570/logo_pagarme.png?v=637599700153200000",
-    description: `
-      Projeto desenvolvido com o intuito de ajudar desenvolvedores
-      com a implementação da API do PagarMe (Pagamentos e transações) com Typescript no NodeJS e 
-      utilizando o banco de dados MongoDB. 
-    `,
-  },
-  {
-    name: "Full Calendar Scheduler",
-    stacks: ["Next", "Typescript", "Css-In-Js", "MongoDB"],
-    link: "https://fullcalendar-scheduler-web.vercel.app/",
-    github: "https://github.com/jrsousadev/fullcalendar-scheduler-web",
-    image: "https://i.imgur.com/f2RukgD.png",
-    description: `Componente inspirado no Google Agenda, o mesmo foi criado
-    com o intuito de ajudar pessoas que necessitem das funcionalidades prontas de listar/criar/atualizar/delete
-    eventos tanto no front-end quanto integrado ao back-end. (Open Source)`,
-  },
-  {
-    name: "Customer Control",
-    stacks: ["Next", "Node", "Typescript", "Css-In-Js", "MongoDB"],
-    link: "https://customer-control-web.herokuapp.com/",
-    github: "https://github.com/jrsousadev/Customer_Control_web",
-    image: "https://i.imgur.com/cD8Wj93.png",
-    description: `Projeto desenvolvido por mim, criado com o intuito de 
-    resolver problemas/dificuldades de amigos de trabalho (e até minha mesmo) 
-    com controle de seus clientes e datas de pagamentos.`,
-  },
-  {
-    name: "Clinica Apit Fhisio",
-    stacks: ["Next", "Node", "Typescript", "Css-In-Js", "MongoDB"],
-    link: "http://clinicaapitfhisio.com.br/professor/login",
-    github: "https://github.com/jrsousadev/Apit_Fhisio_web",
-    image: "https://i.imgur.com/Lw7Ko0h.png",
-    description: `Sistema de clínica (REAL), desenvolvido com o objetivo de cadastrar
-    pacientes, criar matrículas e ter o controle dos vencimentos de pagamento, além de
-    ter os dados dos pacientes e quais são suas matrículas, sistema com autênticação e
-    dados (MongoDB)`,
-  }, */
 ];
